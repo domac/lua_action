bcacdb38dda5e2f6872bc6ac4b5e12c10861cf2c
domac <domacode@gmail.com>
2018-09-26 22:52:29 +0800

    add api gateway

--- basic/api_demo/lualib/api_demo/kafkasend.lua
+++ basic/api_demo/lualib/api_demo/kafkasend.lua
@@ -1,7 +1,6 @@
 local _config = require "api_demo.config"
 local _producer = require "resty.kafka.producer" 
 
-local data = ngx.ctx.messageList 
 
 local logger = ngx.log
 local ERR = ngx.ERR
@@ -11,8 +10,28 @@ local KAFKA_CONFIG = _config.KAFKA_CONFIG
 local topic = _config.TOPIC
 
 
-local kafka_producer = _producer:new(BROKER_LIST, KAFKA_CONFIG)
-local ok, err = kafka_producer:send(topic, key, data)
-if  not ok  then
-    logger(ERR,"kafka send error: "..err)
+local local_producer
+
+local function send_kafka(topic, message)
+    if not local_producer then
+        local_producer = _producer:new(BROKER_LIST, KAFKA_CONFIG)
+    end
+
+    if message then
+        local ok, err = local_producer:send(topic, nil, message)
+        if not ok  then
+            return nil, err
+        end
+        return ok, nil
+    end
+    return false, "message empty"
+end
+
+
+local data = ngx.ctx.messageList 
+
+local ok, err = send_kafka(topic,data)
+
+if not ok then
+    ngx.log(ngx.ERR, "failed to log message to:", err)
 end
