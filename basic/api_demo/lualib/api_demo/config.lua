396369925fc3f30177dec4e8237d2ae8dddbb92e
domac <domacode@gmail.com>
2018-09-26 20:34:12 +0800

    add api gateway

--- basic/api_demo/lualib/api_demo/config.lua
+++ basic/api_demo/lualib/api_demo/config.lua
@@ -1,30 +1,22 @@
---[[
-api config file
-]]
 
 _Config = {
 
     TOPIC = "es-demo",
 
-    -- broker config
     BROKER_LIST = {
         { host = "172.17.0.3", port = 9092 }
     },
 
-    --[[
-        kafka config :
-            one ngx worker one kafka client or producer instance
-    ]]
     KAFKA_CONFIG= {
-        producer_type = "async",            --- async way
-        socket_timeout = 6000,              --- request_timeout ms
-        max_retry = 2,                      --- retry times
-        refresh_interval = 600 * 1000,      --- auto refresh the metadata in milliseconds.
-        keepalive_timeout = 600 * 1000,     --- connection timeout 10 min
-        keepalive_size = 40,                --- pool size for each nginx worker
-        max_buffering = 1000000,            --- queue size
-        flush_time = 100,                   --- send kafka XX ms
-        batch_num = 5                       --- bulk size to send kafka
+        producer_type = "async", 
+        socket_timeout = 6000,
+        max_retry = 2,
+        refresh_interval = 600 * 1000,
+        keepalive_timeout = 600 * 1000,
+        keepalive_size = 40,
+        max_buffering = 1000000,
+        flush_time = 100,
+        batch_num = 500
     }
 }
 
